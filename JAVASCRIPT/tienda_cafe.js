e85cd14bc96f4b2ed43830de75fd56d45c42289a
KeniaKodigo <kenia.paz@kodigo.org>
2023-06-15 10:11:54 -0600

    introduccion a javascript

---
+++ JAVASCRIPT/tienda_cafe.js
@@ -0,0 +1,33 @@
+/**
+ * variables, constantes, tipos de datos, operadores
+ * estructuras de control, funciones
+ */
+
+//let: variable que trabaja local y no se puede sobredeclarar
+//var: variable que trabaja de manera global y se puede sobredeclarar
+
+var nombre = 'kenia'
+nombre; //kenia
+var nombre = 'jose';
+nombre; //jose
+
+let apellido = 'paiz'
+apellido = 'chacon'
+//let apellido 
+function calcularEdad(){
+    let edad
+}
+var dato; //undefined
+let dato2;
+const PI = 3.1416; //float, double
+let numero = "5"; //string
+let suma = 6 + numero;
+suma; //11
+let nulo = null; //null
+let booleano = false; //true 
+
+//asignacion de arreglos
+let frutas = ["uva","manzana","sandia","fresa"];
+console.log(frutas[2]);
+
+
