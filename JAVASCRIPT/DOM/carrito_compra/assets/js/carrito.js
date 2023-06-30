04f4741bf25b632b35e1ddbd13bcd891ce5490c1
KeniaKodigo <kenia.paz@kodigo.org>
2023-06-29 19:32:44 -0600

    carrito de compra - parte 2

--- JAVASCRIPT/DOM/carrito_compra/assets/js/carrito.js
+++ JAVASCRIPT/DOM/carrito_compra/assets/js/carrito.js
@@ -29,7 +29,52 @@ function seleccionarCurso(e){
 
         //parentElement => sacamos el elemento padre de la etiqueta
         console.log(e.target.parentElement.parentElement);
+        const cursoSeleccionado = e.target.parentElement.parentElement;
+        leerDatosCurso(cursoSeleccionado);
+        Swal.fire({
+            position: 'top-center',
+            icon: 'success',
+            title: 'Curso Agregado',
+            showConfirmButton: false,
+            timer: 1500
+        })
     }
+}
+
+//metodo para almacenar los valores del curso en un arreglo
+function leerDatosCurso(curso){
+    //console.log(curso);
+
+    const objetoCurso = {
+        imagen: curso.querySelector('img').src, //capturando la direccion de la imagen
+        titulo: curso.querySelector('h5').textContent, //capturando el texto del h5
+        precio: curso.querySelector('.precio').textContent, //$15.00
+        id: curso.querySelector('a').getAttribute('data-id'), //sacando el valor que hay dentro del data-id
+        cantidad: 1
+    }
+
+    //validar si el curso ya existe en el arreglo
+    //some => metodo que retorna si la condicion se cumple en algun elemento del arreglo y false si no se cumple
+    const existe_curso = arregloCarrito.some(curso => curso.id === objetoCurso.id);
+    console.log(existe_curso);
+    if(existe_curso){
+        //iterando el arreglo para condicionar que curso vamos actualizar
+        arregloCarrito.map(curso => {
+            if(curso.id === objetoCurso.id){
+                //incrementamos la cantidad del curso que ya existe y retornamos toda su informacion
+                curso.cantidad += 1;
+                return curso;
+            }else{
+                //retornamos la informacion de los demas cursos
+                return curso;
+            }
+        })
+    }else{
+        arregloCarrito.push(objetoCurso);
+    }
+    //agregando el curso al arreglo
+    //push() => agrega un elemento al arreglo (lo agrega de ultimo)
+    console.table(arregloCarrito);
 }
 
 function guardarPedido(){
---
+++ JAVASCRIPT/DOM/carrito_compra/assets/js/practica.js
@@ -0,0 +1,34 @@
+
+let arreglo = [2,3,4,5,23,19,27];
+
+//some() => true, false (||)
+
+//every() => true, false (&&)
+
+arreglo.every(numero => numero > 15); //false
+arreglo.some(numero => numero > 15); //true
+
+
+let carrito = [
+    {
+        imagen: "",
+        titulo: "html",
+        id: "1",
+        cantidad: "2"
+    },
+    {
+        imagen: "",
+        titulo: "html",
+        id: "4"
+    },
+    {
+        imagen: "",
+        titulo: "html",
+        id: "6"
+    },
+    {
+        imagen: "",
+        titulo: "html",
+        id: "1"
+    },
+]
